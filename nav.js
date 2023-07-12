a7d8074ab9f7afa29b402ed02321c608caddf798
01000001 <68795918+01000001-01101011@users.noreply.github.com>
2023-07-12 18:34:24 -0400

    thingy

--- nav.js
+++ nav.js
@@ -2,10 +2,11 @@ window.onload = (e) => {
 
     var div = document.createElement('div'), // Create a new div
     script = document.scripts[document.scripts.length - 1]; // A reference to the currently running script
+    div.id = "nav";
+    script.parentElement.insertBefore(div, script); // Add the newly-created div to the page
 
     
-    div.load("./nav.html"); // Add some content to the newly-created div
-    script.parentElement.insertBefore(div, script); // Add the newly-created div to the page
+    $("#nav").load("./nav.html"); // Add some content to the newly-created div
 
 
     //Straight from stackoverflow :P
